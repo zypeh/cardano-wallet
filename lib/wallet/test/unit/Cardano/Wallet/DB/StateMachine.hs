8ec30fe3787c33599774b707dc4c456688a20040
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-12-02 01:51:01 +0000

    Remove unused instance of `ToExpr` for `NonEmptyMap`.

--- lib/wallet/test/unit/Cardano/Wallet/DB/StateMachine.hs
+++ lib/wallet/test/unit/Cardano/Wallet/DB/StateMachine.hs
@@ -218,8 +218,6 @@ import Data.List.Extra
     ( enumerate )
 import Data.Map
     ( Map )
-import Data.Map.Strict.NonEmptyMap
-    ( NonEmptyMap )
 import Data.Maybe
     ( catMaybes, fromJust, isJust, isNothing, mapMaybe )
 import Data.Quantity
@@ -930,9 +928,6 @@ instance Traversable t => Rank2.Traversable (At t) where
 deriving instance ToExpr s => ToExpr (Model s Concrete)
 
 instance ToExpr s => ToExpr (Mock s) where
-    toExpr = genericToExpr
-
-instance (ToExpr k, ToExpr v) => ToExpr (NonEmptyMap k v) where
     toExpr = genericToExpr
 
 instance ToExpr WalletId where
